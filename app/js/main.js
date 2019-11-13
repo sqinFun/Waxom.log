3eb6bea4742ed3a2ad2b1718b74705b8bbe8ac0b
sqinFun <denchyg@yandex.ru>
2019-11-13 21:24:55 +0300

    add navigation and first section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,15 @@
 $(function(){
-
-
+    $('.main-slider').slick({
+        dots:true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        draggable:false,
+        prevArrow: '.slider-arrow__prev',
+        nextArrow: '.slider-arrow__next',
+    })
+    
 
   
 });
